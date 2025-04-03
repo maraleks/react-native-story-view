da35b395b9575640a12a54c421060b262d76d40e
Aleksey Martynov <job@maraleks.ru>
2025-04-03 11:25:30 +0300

    Replaced react-native-video with expo-av

--- src/components/StoryView/StoryView.tsx
+++ src/components/StoryView/StoryView.tsx
@@ -1,43 +1,31 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { ActivityIndicator, useWindowDimensions, View } from 'react-native';
-import Video, { OnBufferData, OnLoadData } from 'react-native-video';
-import convertToProxyURL from 'react-native-video-cache-control';
+import { Audio, Video } from 'expo-av';
 import { Colors, Metrics } from '../../theme';
 import ProgressiveImage from './ProgressiveImage';
 import styles from './styles';
 import { StoryViewProps, StroyTypes } from './types';
-
-const BUFFER_TIME = 1000 * 60;
 
 const StoryView = (props: StoryViewProps) => {
   const [loading, setLoading] = useState(true);
   const [buffering, setBuffering] = useState(true);
   const source = props?.stories?.[props?.progressIndex];
   const videoRef = useRef<Video>(null);
-  const videoData = useRef<OnLoadData>();
+  const videoData = useRef<Audio.VideoStatus>();
   const isCurrentIndex = props?.index === props?.storyIndex;
 
   useEffect(() => {
-    if (props?.index === props?.storyIndex) {
-      videoRef?.current?.seek(0);
+    if (isCurrentIndex) {
+      videoRef.current?.setPositionAsync(0); // Сброс видео при переключении
     }
   }, [props?.storyIndex, props?.index]);
 
-  const onLoadStart = () => {
-    setLoading(true);
-  };
-
   const loadVideo = () => {
-    if (isCurrentIndex) {
-      if (videoData.current === undefined) return;
+    if (isCurrentIndex && videoData.current) {
       setLoading(false);
       setBuffering(false);
       props?.onVideoLoaded?.(videoData.current);
     }
-  };
-
-  const onBuffer = (data: OnBufferData) => {
-    setBuffering(data.isBuffering);
   };
 
   const { height, width } = useWindowDimensions();
@@ -56,45 +44,35 @@ const StoryView = (props: StoryViewProps) => {
           <>
             <Video
               ref={videoRef}
-              resizeMode="contain"
-              paused={props.pause || loading}
-              source={{
-                uri: convertToProxyURL({
-                  url: source?.url!,
-                }),
-              }}
-              onEnd={props?.onVideoEnd}
-              onError={(_error: any) => {
-                setLoading(false);
-              }}
-              onProgress={data => {
-                if (isCurrentIndex) {
-                  props?.onVideoProgress?.(data);
+              resizeMode={Video.RESIZE_MODE_CONTAIN}
+              isLooping={false}
+              shouldPlay={!props.pause && !loading}
+              source={{ uri: source?.url! }} // URL без проксирования
+              onPlaybackStatusUpdate={(status) => {
+                videoData.current = status;
+                if (status.isLoaded) {
+                  if (status.didJustFinish) {
+                    props?.onVideoEnd?.();
+                  }
+                  props?.onVideoProgress?.({
+                    currentTime: status.positionMillis,
+                    playableDuration: status.playableDurationMillis,
+                    seekableDuration: status.durationMillis || 0,
+                  });
+                  if (!status.isBuffering) loadVideo();
                 }
+                setBuffering(status.isBuffering || false);
               }}
-              bufferConfig={{
-                minBufferMs: BUFFER_TIME,
-                bufferForPlaybackMs: BUFFER_TIME,
-                bufferForPlaybackAfterRebufferMs: BUFFER_TIME,
-              }}
-              onBuffer={onBuffer}
-              onLoadStart={onLoadStart}
-              onLoad={(item: OnLoadData) => {
-                videoData.current = item;
-                !Metrics.isIOS && loadVideo();
-              }}
-              onReadyForDisplay={loadVideo}
+              useNativeControls={false}
               style={styles.contentVideoView}
               {...props?.videoProps}
             />
             {(loading || buffering) && props?.showSourceIndicator && (
               <ActivityIndicator
                 animating
-                pointerEvents="none"
                 color={Colors.loaderColor}
                 size="small"
                 style={styles.loaderView}
-                {...props?.sourceIndicatorProps}
               />
             )}
           </>
